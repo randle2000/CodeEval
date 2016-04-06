7030c3f45e2c7b8a4d8ebe6dad87863b07566310
Randle McMurphy <jmarton2000@gmail.com>
2016-04-06 23:30:51 +0300

    Added SumOfIntegersFromFile

--- Easy/SumOfIntegersFromFile/src/Main.java
+++ Easy/SumOfIntegersFromFile/src/Main.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Stream;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        Stream<String> lines = Files.lines(new File(args[0]).toPath());
+        int sum = lines
+                .filter(line -> line.length()!=0)
+                .mapToInt(Integer::parseInt)
+                .sum();
+        System.out.println(sum);
     }
 }
