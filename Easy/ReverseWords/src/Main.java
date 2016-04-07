96ca19f34ec8f021d8c24104d4d62fad3bde98cb
Randle McMurphy <jmarton2000@gmail.com>
2016-04-07 14:44:57 +0300

    Added ReverseWords

---
+++ Easy/ReverseWords/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(new File(args[0]).toPath(), Charset.defaultCharset() );
+        for (String ln : lines) {
+            List<String> strItems = Arrays.asList(ln.split("\\s+"));
+            Collections.reverse(strItems);
+            String output = strItems.stream().collect(Collectors.joining(" "));
+            System.out.println(output);
+        }
+    }
+}
