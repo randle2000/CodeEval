75265b22f1927e3ecfddbc1a702755e3d0e5900e
Randle McMurphy <jmarton2000@gmail.com>
2016-04-07 15:12:17 +0300

    Provisioned for empty lines in ReverseWords' input

--- Easy/ReverseWords/src/Main.java
+++ Easy/ReverseWords/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(new File(args[0]).toPath(), Charset.defaultCharset() );
         for (String ln : lines) {
+            if (ln.length() == 0) continue;
             List<String> strItems = Arrays.asList(ln.split("\\s+"));
             Collections.reverse(strItems);
             String output = strItems.stream().collect(Collectors.joining(" "));
